cfd4a33d2f729a0c89e516aa49e2684dd2c29322
ivansimeoni <ivanspy@protonmail.com>
2025-05-13 23:20:31 +0200

    Inicio gestor de tareas

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+titulo = input("Ingrese un Titulo")
+descripcion = input("Ingrese la descripcion")
+fecha = input("ingrese la fecha limite")
+
+agenda= {}
+
+
+agenda[titulo] = {
+    "Descripcion": descripcion,
+    "Fecha Limite": fecha
+    }
+
+
